b74c05100e34255771190c5562fed1d297745b25
ohshitimsorry <47817837+ohshitimsorry@users.noreply.github.com>
2019-05-26 12:39:51 +0300

    Add files via upload

---
+++ brackets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brackets
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                int brackets = 0;
+                int i;
+                for (i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '(')
+                    {
+                        brackets++;
+                    }
+                    else if (s[i] == ')' && --brackets < 0)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine(brackets);// if the brackets are arranged correctly the top line displays 0 if not then -1
+                Console.WriteLine(i);// the bottom line shows the number of correctly placed brackets
+            }
+        }
+    }
+}
